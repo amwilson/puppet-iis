3127f0acb35217805cb2aaf961831a2224c3bd46
Drew Wilson <dwilson@opentable.com>
2013-11-14 16:25:33 +0000

    Update manage_site to update sites physical path and app pools when config changes

--- manifests/manage_site.pp
+++ manifests/manage_site.pp
@@ -7,23 +7,36 @@ define iis::manage_site($site_path, $app_pool, $host_header = '', $site_name = $
     site_path => $site_path
   }
 
+  $cmdSiteExists = "Test-Path \\\"IIS:\\Sites\\${site_name}\\\""
+
   $createSwitches = ["-Name \\\"${site_name}\\\"",
-        "-Port ${port} -IP ${ip_address}",
-        "-HostHeader \\\"${host_header}\\\"",
-        "-PhysicalPath \\\"${site_path}\\\"",
-        "-ApplicationPool \\\"${app_pool}\\\"",
-        "-Ssl:$${ssl}"]
+                      "-Port ${port} -IP ${ip_address}",
+                      "-HostHeader \\\"${host_header}\\\"",
+                      "-PhysicalPath \\\"${site_path}\\\"",
+                      "-ApplicationPool \\\"${app_pool}\\\"",
+                      "-Ssl:$${ssl}"]
 
   $switches = join($createSwitches,' ')
+
   exec { "CreateSite-${site_name}" :
     command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; New-WebSite ${switches} \"",
     path      => "${iis::param::powershell::path};${::path}",
-    onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((Test-Path \\\"IIS:\\Sites\\${site_name}\\\")) { exit 1 } else { exit 0 }\"",
+    onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if(${$cmdSiteExists}) { exit 1 } else { exit 0 }\"",
     logoutput => true,
     require   => [ Iis::Createpath["${site_name}-${site_path}"], Iis::Manage_app_pool[$app_pool] ],
   }
 
-  exec { "Set-PhysicalPath-${site_name}":
+  exec { "UpdateSite-PhysicalPath-${site_name}":
+    command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; Set-ItemProperty \\\"IIS:\\Sites\\${site_name}\\\" -Name physicalPath -Value \\\"${site_path}\\\"\"",
+    onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((${$cmdSiteExists}) -eq \$true -and (!(Get-ItemProperty \\\"IIS:\\Sites\\${site_name}\\\" physicalPath) -eq \\\"${site_path}\\\")) { exit 1 } else { exit 0 }\"",
+    path      => "${iis::param::powershell::path};${::path}",
+    logoutput => true,
+    require   => Exec["CreateSite-${site_name}"],
+  }
+
+  exec { "UpdateSite-ApplicationPool-${site_name}":
+    command   => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; Set-ItemProperty \\\"IIS:\\Sites\\${site_name}\\\" -Name applicationPool -Value \\\"${app_pool}\\\"\"",
+    onlyif    => "${iis::param::powershell::command} -Command \"Import-Module WebAdministration; if((${$cmdSiteExists}) -eq \$true -and (!(Get-ItemProperty \\\"IIS:\\Sites\\${site_name}\\\" applicationPool) -eq \\\"${app_pool}\\\")) { exit 1 } else { exit 0 }\"",
     path      => "${iis::param::powershell::path};${::path}",
     logoutput => true,
     require   => Exec["CreateSite-${site_name}"],
--- spec/defines/manage_site_spec.rb
+++ spec/defines/manage_site_spec.rb
@@ -15,7 +15,7 @@ describe 'iis::manage_site', :type => :define do
 
     it { should contain_exec('CreateSite-myWebSite').with({
       'command' => "#{powershell} -Command \"Import-Module WebAdministration; New-WebSite -Name \\\"myWebSite\\\" -Port 80 -IP * -HostHeader \\\"myHost.example.com\\\" -PhysicalPath \\\"C:\\inetpub\\wwwroot\\myWebSite\\\" -ApplicationPool \\\"myAppPool.example.com\\\" -Ssl:$false \"",
-      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if((Test-Path \\\"IIS:\\Sites\\myWebSite\\\")) { exit 1 } else { exit 0 }\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if(Test-Path \\\"IIS:\\Sites\\myWebSite\\\") { exit 1 } else { exit 0 }\"",
     })}
   end
 
@@ -33,7 +33,17 @@ describe 'iis::manage_site', :type => :define do
 
     it { should contain_exec('CreateSite-myWebSite').with({
       'command' => "#{powershell} -Command \"Import-Module WebAdministration; New-WebSite -Name \\\"myWebSite\\\" -Port 1080 -IP 127.0.0.1 -HostHeader \\\"myHost.example.com\\\" -PhysicalPath \\\"C:\\inetpub\\wwwroot\\path\\\" -ApplicationPool \\\"myAppPool.example.com\\\" -Ssl:$false \"",
-      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if((Test-Path \\\"IIS:\\Sites\\myWebSite\\\")) { exit 1 } else { exit 0 }\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if(Test-Path \\\"IIS:\\Sites\\myWebSite\\\") { exit 1 } else { exit 0 }\"",
+    })}
+
+    it { should contain_exec('UpdateSite-PhysicalPath-myWebSite').with({
+      'command' => "#{powershell} -Command \"Import-Module WebAdministration; Set-ItemProperty \\\"IIS:\\Sites\\myWebSite\\\" -Name physicalPath -Value \\\"C:\\inetpub\\wwwroot\\path\\\"\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if((Test-Path \\\"IIS:\\Sites\\myWebSite\\\") -eq \$true -and (!(Get-ItemProperty \\\"IIS:\\Sites\\myWebSite\\\" physicalPath) -eq \\\"C:\\inetpub\\wwwroot\\path\\\")) { exit 1 } else { exit 0 }\"",
+    })}
+
+    it { should contain_exec('UpdateSite-ApplicationPool-myWebSite').with({
+      'command' => "#{powershell} -Command \"Import-Module WebAdministration; Set-ItemProperty \\\"IIS:\\Sites\\myWebSite\\\" -Name applicationPool -Value \\\"myAppPool.example.com\\\"\"",
+      'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if((Test-Path \\\"IIS:\\Sites\\myWebSite\\\") -eq \$true -and (!(Get-ItemProperty \\\"IIS:\\Sites\\myWebSite\\\" applicationPool) -eq \\\"myAppPool.example.com\\\")) { exit 1 } else { exit 0 }\"",
     })}
   end
 end
